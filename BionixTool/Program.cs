52876cc6e01d8015daf2e8939913130cb67b0540
asecahusada <42400028+asecahusada@users.noreply.github.com>
2018-09-14 11:16:44 +1000

    Update Program.cs

--- BionixTool/Program.cs
+++ BionixTool/Program.cs
@@ -17,7 +17,9 @@ namespace BionixTool
             CreateWebHostBuilder(args).Build().Run();
             //Added a new line in VS 2017
             // New line BXT-127 asecahusada
-            // New line added directly into BIONIX_ENHANCEMENTS asecahusada
+            // New line added directly into BIONIX_ENHANCEMENTS asecahusada 
+            // New line added directly into BIONIX_ENHANCEMENTS asecahusada #2
+            // New line added directly into BIONIX_ENHANCEMENTS asecahusada #3
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
